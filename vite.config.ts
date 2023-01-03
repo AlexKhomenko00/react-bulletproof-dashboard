57e8a15c1fadaf9fd52b997933ece79d56a92815
Alex Khomenko <oleksandr.khomenko2000@gmail.com>
2023-01-03 22:38:06 +0100

    Minor amendments for configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,18 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
   plugins: [
     react(),
     checker({
       typescript: true,
       eslint: {
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        lintCommand: 'eslint "src/**/*.{ts,tsx}"',
       },
       stylelint: {
-        lintCommand: "stylelint ./src/**/*.css",
+        lintCommand: "stylelint src/**/*.css",
       },
     }),
   ],
